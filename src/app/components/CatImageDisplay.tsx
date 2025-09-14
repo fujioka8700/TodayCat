46f97e9e94b7c1a422cf13db70eac4c0308110ce
MasahiroFujioka <fujioka8700@gmail.com>
2025-09-15 08:58:37 +0900

    It changed from img to Image.

--- src/app/components/CatImageDisplay.tsx
+++ src/app/components/CatImageDisplay.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type CatImageDisplayProps = {
     imageUrl: string | null;
     className?: string; // プロパティを追加し、?で省略可能にする
@@ -7,11 +9,12 @@ const CatImageDisplay = ({imageUrl, className}:CatImageDisplayProps) => {
     if (imageUrl) {
         return (
             <div className={className}>
-                <img 
+                <Image 
                     src={imageUrl} 
                     alt="猫の画像を表示します。" 
                     width={500} 
-                    height="auto" 
+                    height={500} 
+                    className="rounded-lg"
                 />
             </div>
         );
