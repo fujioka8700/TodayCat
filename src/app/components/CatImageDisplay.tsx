0597b81f85618c6443e0fbf0d425bdc87e4701ac
MasahiroFujioka <fujioka8700@gmail.com>
2025-09-15 04:57:39 +0900

    It added margin to cat image.

--- src/app/components/CatImageDisplay.tsx
+++ src/app/components/CatImageDisplay.tsx
@@ -1,16 +1,17 @@
 type CatImageDisplayProps = {
     imageUrl: string | null;
+    className?: string; // プロパティを追加し、?で省略可能にする
 };
 
-const CatImageDisplay = ({imageUrl}:CatImageDisplayProps) => {
+const CatImageDisplay = ({imageUrl, className}:CatImageDisplayProps) => {
     if (imageUrl) {
         return (
-            <div>
+            <div className={className}>
                 <img 
                     src={imageUrl} 
                     alt="猫の画像を表示します。" 
                     width={500} 
-                    height={500} 
+                    height="auto" 
                 />
             </div>
         );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-2xl">猫画像アプリ</h1>
-      <CatImageDisplay imageUrl={catImageUrl}/>
+      <CatImageDisplay imageUrl={catImageUrl} className="my-4"/>
       <MyButton onClick={handleClick}/>
     </div>
   );
