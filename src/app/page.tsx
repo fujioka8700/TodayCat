b5ced2bdcae39b4728f17aebf600666a699cae01
MasahiroFujioka <fujioka8700@gmail.com>
2025-09-15 18:56:43 +0900

    It added a Loading image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
 import CatImageDisplay from './components/CatImageDisplay';
 import { MyButton } from './components/MyButton';
 
@@ -13,6 +14,7 @@ interface SearchCatImage {
 
 export default function Home() {
   const [catImageUrl, setCatImageUrl] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchCatImage = async (): Promise<SearchCatImage> => {
     const res = await fetch('https://api.thecatapi.com/v1/images/search');
@@ -21,6 +23,7 @@ export default function Home() {
   };
 
   const handleClick = async (): Promise<void> => {
+    setIsLoading(true);
     try {
       const catImage: SearchCatImage = await fetchCatImage();
       setCatImageUrl(catImage.url);
@@ -29,6 +32,7 @@ export default function Home() {
       // エラー時にも`null`をセット
       setCatImageUrl(null);
     }
+    setIsLoading(false);
   };
 
   // useEffectフックは、コンポーネントが最初にマウントされた時、一度だけ実行されます。
@@ -39,7 +43,11 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-2xl mb-4">猫画像アプリ</h1>
-      <CatImageDisplay imageUrl={catImageUrl} />
+      {isLoading ? (
+        <ClipLoader loading color="#808080" size={50} />
+      ) : (
+        <CatImageDisplay imageUrl={catImageUrl} />
+      )}
       <MyButton onClick={handleClick} className="mt-4" />
     </div>
   );
